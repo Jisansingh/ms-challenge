ecff0ae2fbe0372bb89cba4e232700e40686c38e
Jisansingh <jisansingh@gmail.com>
2025-08-21 03:07:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Change heading text on load
+window.onload = () => {
+  const heading = document.getElementById("main-heading");
+  heading.textContent = "Welcome, dhruv and jisan!";
+  console.log("Congrats 🎉 You’re closer to the password!");
+};
